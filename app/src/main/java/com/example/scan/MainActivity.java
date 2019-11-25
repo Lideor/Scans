4d708ef0c988a7e128b9df512fec636255cdd9a3
Lideor <Lideor@yandex.ru>
2019-11-25 05:12:02 +0500

    Зачем

---
+++ app/src/main/java/com/example/scan/MainActivity.java
@@ -0,0 +1,290 @@
+package com.example.scan;
+import android.Manifest;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+
+//асинхрон
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.content.Context;
+
+import android.content.Intent;
+//сохранение текста
+
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+// работа с инетом
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+//работа с локацией
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    int login_id = -1;//логин пользователя
+    SharedPreferences sPref;// файл с настройками
+    String url = "http://www.zaural-vodokanal.ru/php/get_pos.php"; // отправка локации
+    final int REQUEST_CODE_ACCESS_FINE_LOCATION = 200; // код ддя проверки разрешения на
+    TextView MainText; // бокс основного текста
+    private LocationManager locationManager;// локация
+    Context Ctn = this;
+    Location location;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MainText =(TextView) findViewById(R.id.MainText);
+        if (loadLogin_id() == 0) {
+            Intent intent = new Intent(MainActivity.this, New_login.class);
+
+            startActivity(intent);
+        }
+         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        int permissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000 * 1, 10, locationListener);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_ACCESS_FINE_LOCATION);
+
+        }
+
+    }
+
+    void StartLocation(){
+        int permissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000 * 1, 10, locationListener);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_ACCESS_FINE_LOCATION);
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("asdasdasdasdasdasdasdasdasd");
+
+
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        int permissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                1000 * 1, 10, locationListener);
+        new RequestTask().execute("http://www.zaural-vodokanal.ru/php/get_pos.php");
+
+    }
+    //работа с разрешениями
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ACCESS_FINE_LOCATION:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted
+                    ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                            1000 * 10, 10, locationListener);
+                } else {
+                    // permission denied
+                }
+                return;
+        }
+    }
+
+    //получение гпс2
+    private LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location loc) {
+            location=loc;
+            showLocation(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            checkEnabled();
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            checkEnabled();
+            int permissionStatus = ContextCompat.checkSelfPermission(Ctn, Manifest.permission.ACCESS_FINE_LOCATION);
+            showLocation(locationManager.getLastKnownLocation(provider));
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            if (provider.equals(LocationManager.GPS_PROVIDER)) {
+                //MainText.setText("Status: " + String.valueOf(status));
+            } else if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
+             //   MainText.setText("Status: " + String.valueOf(status));
+            }
+        }
+    };
+
+    private void showLocation(Location location) {
+        if (location == null)
+            return;
+        if (location.getProvider().equals(LocationManager.GPS_PROVIDER)) {
+            MainText.setText(formatLocation(location));
+        } else if (location.getProvider().equals(
+                LocationManager.NETWORK_PROVIDER)) {
+            MainText.setText(formatLocation(location));
+        }
+    }
+
+    private String formatLocation(Location location) {
+        if (location == null)
+            return "";
+        System.out.println("----------------------------");
+        System.out.println(String.format(
+                "Coordinates: lat = %1$.4f, lon = %2$.4f, time = %3$tF %3$tT",
+                location.getLatitude(), location.getLongitude(), new Date(
+                        location.getTime())));
+        return String.format(
+                "Coordinates: lat = %1$.4f, lon = %2$.4f, time = %3$tF %3$tT",
+                location.getLatitude(), location.getLongitude(), new Date(
+                        location.getTime()));
+    }
+
+    private void checkEnabled() {
+        MainText.setText("Enabled: "
+                + locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER));
+        MainText.setText("Enabled: "
+                + locationManager
+                .isProviderEnabled(LocationManager.NETWORK_PROVIDER));
+    }
+
+    public void onClickLocationSettings(View view) {
+        startActivity(new Intent(
+                android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+    };
+
+    int loadLogin_id() {
+        sPref = getSharedPreferences("prefs",MODE_PRIVATE);
+        login_id = sPref.getInt("login_id", -1);
+        if (login_id == -1) return 0;
+        else return 1;
+
+    }
+
+    // меню сетиинг
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            sPref = getPreferences(MODE_PRIVATE);
+            Intent intent = new Intent(MainActivity.this, New_login.class);
+            SharedPreferences.Editor ed = sPref.edit();
+            ed.putInt("login_id", -1);
+            ed.apply();
+            startActivity(intent);
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    class RequestTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+               /* //создаем запрос на сервер
+                DefaultHttpClient hc = new DefaultHttpClient();
+                ResponseHandler<String> res = new BasicResponseHandler();
+                //он у нас будет посылать post запрос
+                HttpPost postMethod = new HttpPost(url);
+                //будем передавать два параметра
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+                //передаем параметры из наших текстбоксов
+                //лоигн
+                nameValuePairs.add(new BasicNameValuePair("login_name", params[0]));
+                //пароль
+                //собераем их вместе и посылаем на сервер
+                postMethod.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                //получаем ответ от сервера
+                String response = hc.execute(postMethod, res);
+                if (response != "not") {
+                    int num = Integer.parseInt(response);
+                    sPref = getPreferences(MODE_PRIVATE);
+                    SharedPreferences.Editor ed = sPref.edit();
+                    ed.putInt("login_id", num);
+                    ed.apply();
+                    finish();
+                }
+                //посылаем на вторую активность полученные параметры
+                */
+
+               int i=0;
+               while (i!=-1) {
+                   location.getLatitude();
+                   System.out.print("---------------------");
+                   System.out.print(location.getLatitude());
+                   System.out.println(i);
+                    i++;
+                    Thread.sleep(3000);
+                   }
+               } catch (Exception e) {
+                System.out.println("Exp=" + e);
+            }
+            return null;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/scan/New_login.java
@@ -0,0 +1,94 @@
+package com.example.scan;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import java.util.ArrayList;
+import java.util.List;
+
+public class New_login extends Activity {
+
+    SharedPreferences sPref;//Настройка
+    private MenuItem logoutMI;
+    String url = "http://www.zaural-vodokanal.ru/php/auth.php";
+
+    Button btnReg; //кнопнка регестрации
+    EditText loginName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_login);
+        btnReg = (Button) findViewById(R.id.btnReg);
+        loginName = (EditText) findViewById(R.id.NewLogin);
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.print("asddddddddasdasddddddddd=");
+                if (loginName.getText().toString() != "") {
+                    System.out.print("asdddddddddasdasd");
+                    new RequestTask().execute(loginName.getText().toString());
+                } else {
+                    System.out.print("asddddddddddddddddddddddddddddd=");
+                    System.out.print(loginName.getText().toString());
+                    loginName.setText("adsasd");
+                }
+            }
+        });
+    }
+        class RequestTask extends AsyncTask<String, String, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                try {
+                    //создаем запрос на сервер
+                    DefaultHttpClient hc = new DefaultHttpClient();
+                    ResponseHandler<String> res = new BasicResponseHandler();
+                    //он у нас будет посылать post запрос
+                    HttpPost postMethod = new HttpPost(url);
+                    //будем передавать два параметра
+                    List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+                    //передаем параметры из наших текстбоксов
+                    //лоигн
+                    nameValuePairs.add(new BasicNameValuePair("login_name",params[0]));
+                    //пароль
+                    //собераем их вместе и посылаем на сервер
+                    postMethod.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                    //получаем ответ от сервера
+                    String response = hc.execute(postMethod, res);
+                    if (response!="not")
+                    {
+                        int num = Integer.parseInt(response);
+                        sPref = getSharedPreferences("prefs",MODE_PRIVATE);
+                        SharedPreferences.Editor ed = sPref.edit();
+                        ed.putInt("login_id",num);
+                        ed.apply();
+                        finish();
+                    }
+                    //посылаем на вторую активность полученные параметры
+
+                } catch (Exception e) {
+                    System.out.println("Exp=" + e);
+                }
+                return null;
+            }
+    }
+}
